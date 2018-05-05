08c4146e70683b02030d7d1abbc35524f4162457
Rafaël De Jongh <rafael_boba_fett@msn.com>
2018-05-05 22:08:36 +0200

    Re-organisiation + packaging + dropdown

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import Vue from 'vue'
 import App from './App'
+//Modules
+import BackToTop from 'vue-backtotop'
+import vueSmoothScroll from 'vue-smoothscroll'
+import VueCollapse from 'vue2-collapse'
+Vue.use(BackToTop);
+Vue.use(vueSmoothScroll);
+Vue.use(VueCollapse);
+//Production Tips
 Vue.config.productionTip = true
+//Application
 new Vue({
 	el:'#app',
 	components:{App},
