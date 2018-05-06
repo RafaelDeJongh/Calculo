f41b7b01aee04ca0eb9610e4285d542c206c2cc4
Rafaël De Jongh <rafael_boba_fett@msn.com>
2018-05-06 16:30:02 +0200

    Changed some price values and optimised code.

--- src/App.vue
+++ src/App.vue
@@ -198,7 +198,7 @@ export default{
 			},
 			options:{
 				sitetype:[
-					{text:"Standard",         price:{low:600, high:1000}},
+					{text:"Standard",         price:{low:550, high:750}},
 					{text:"E-Commerce",       price:{low:2000,high:4000}},
 					{text:"Custom Production",price:{low:5000,high:8000}}
 				],
@@ -209,21 +209,21 @@ export default{
 				speed:[
 					{text:"Less than 1 month",   price:{low:1000,high:3000}},
 					{text:"Around 1 to 2 months",price:{low:500, high:1000}},
-					{text:"More than 2 months",  price:{low:250, high:500}}
+					{text:"More than 2 months",  price:{low:300, high:500}}
 				],
 				features:[
 					{id:"seo",         price:{low:250,high:500}, text:"Search Engine Optimisation (SEO)"},
-					{id:"smo",         price:{low:400,high:800}, text:"Social Media Optimisation (SMO)"},
+					{id:"smo",         price:{low:350,high:700}, text:"Social Media Optimisation (SMO)"},
 					{id:"security",    price:{low:500,high:1000},text:"Security Setup"},
-					{id:"caching",     price:{low:700,high:1400},text:"Caching & Compression Setup"},
-					{id:"cdn",         price:{low:200,high:400}, text:"Content Delivery Network (CDN) Setup"},
-					{id:"server",      price:{low:350,high:700}, text:"Server/Webhost Setup"},
+					{id:"caching",     price:{low:500,high:1000},text:"Caching & Compression Setup"},
+					{id:"cdn",         price:{low:150,high:300}, text:"Content Delivery Network (CDN) Setup"},
+					{id:"server",      price:{low:400,high:800}, text:"Server/Webhost Setup"},
 					{id:"mail",        price:{low:150,high:300}, text:"Mail Setup"},
 					{id:"gallery",     price:{low:250,high:500}, text:"Image/Sliders Gallery"},
 					{id:"pagebuilder", price:{low:150,high:300}, text:"PageBuilder Configuration"},
 					{id:"contactform", price:{low:150,high:300}, text:"Contact Form"},
 					{id:"multilingual",price:{low:500,high:1000},text:"Multilingual Support"},
-					{id:"backup",      price:{low:250,high:500}, text:"Backup (Offsite/Server side backup configuration)"}
+					{id:"backup",      price:{low:200,high:400}, text:"Backup (Offsite/Server side backup configuration)"}
 				]
 			}
 		}
@@ -246,8 +246,9 @@ export default{
 					}else{
 					//Calculate Options
 						for(var index in this.options[option]){
-							if(this.options[option][index].text == this.form[option]){
-								otherPrice += isMin ? this.options[option][index].price.low : this.options[option][index].price.high;
+						var getOption = this.options[option][index];
+							if(getOption.text == this.form[option]){
+								otherPrice += isMin ? getOption.price.low : getOption.price.high;
 							}
 						}
 					}
